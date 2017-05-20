116ce0fe7165780ead83f30ec882746e2bd1b706
Konrad Kühne <konnykuehne@gmail.com>
2017-05-20 21:47:39 +0200

    use basic react skeleton

---
+++ app/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      value: null
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+        </div>
+        <div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
